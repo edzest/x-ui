3b5d3087f78e2ffc0585618858c3fba3cea7199e
Piyush Ranjan <piyushranjan95@gmail.com>
2024-07-16 13:44:02 +0530

    add links to home page

--- src/common/Footer.js
+++ src/common/Footer.js
@@ -1,36 +1,39 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Footer() {
     return (
         <div className='bg-base-200'>
-                <footer className="footer bg-base-200 text-base-content p-10 container mx-auto">
-                    <aside>
+            <footer className="footer bg-base-200 text-base-content p-10 container mx-auto">
+                <aside>
+                    <Link to="https://www.edzest.org/" target="_blank">
                         <img alt="logo" src="/edzest_logo.webp" width="100" />
-                        <p>
-                            <b>Edzest Education Services (OPC) Pvt.Ltd</b>
-                            <br />
-                            SY no. 42, Near J R Layout Haralur, <br />
-                            Bengaluru- 560102, Karnataka, India
-                        </p>
-                    </aside>
-                    <nav>
-                        <h6 className="footer-title">Company</h6>
-                        <a className="link link-hover" href="https://www.edzest.org" target='_blank' rel="noreferrer">Edzest Website</a>
-                        <a className="link link-hover" href="https://courses.edzest.org/" target='_blank' rel="noreferrer">PMP training courses</a>
-                    </nav>
-                    <nav>
-                        <h6 className="footer-title">Contact</h6>
-                        <p>+91 9673332684</p>
-                        <a className="link link-hover" href='mailto:contact@edzest.org'>contact@edzest.org</a>
-                    </nav>
-                    <nav>
-                        <h6 className="footer-title">Social</h6>
-                        <a className="link link-hover" href='https://www.linkedin.com/company/edzest/' target='_blank' rel="noreferrer">LinkedIn</a>
-                        <a className="link link-hover" href='https://www.instagram.com/edzest_org/' target='_blank' rel="noreferrer">Instagram</a>
-                        <a className="link link-hover" href='https://www.youtube.com/c/EdzestEducationServices' target='_blank' rel="noreferrer">Youtube</a>
-                    </nav>
-                </footer>
-            </div>
+                    </Link>
+                    <p>
+                        <b>Edzest Education Services (OPC) Pvt.Ltd</b>
+                        <br />
+                        SY no. 42, Near J R Layout Haralur, <br />
+                        Bengaluru- 560102, Karnataka, India
+                    </p>
+                </aside>
+                <nav>
+                    <h6 className="footer-title">Company</h6>
+                    <a className="link link-hover" href="https://www.edzest.org" target='_blank' rel="noreferrer">Edzest Website</a>
+                    <a className="link link-hover" href="https://courses.edzest.org/" target='_blank' rel="noreferrer">PMP training courses</a>
+                </nav>
+                <nav>
+                    <h6 className="footer-title">Contact</h6>
+                    <p>+91 9673332684</p>
+                    <a className="link link-hover" href='mailto:contact@edzest.org'>contact@edzest.org</a>
+                </nav>
+                <nav>
+                    <h6 className="footer-title">Social</h6>
+                    <a className="link link-hover" href='https://www.linkedin.com/company/edzest/' target='_blank' rel="noreferrer">LinkedIn</a>
+                    <a className="link link-hover" href='https://www.instagram.com/edzest_org/' target='_blank' rel="noreferrer">Instagram</a>
+                    <a className="link link-hover" href='https://www.youtube.com/c/EdzestEducationServices' target='_blank' rel="noreferrer">Youtube</a>
+                </nav>
+            </footer>
+        </div>
     );
 }
 
--- src/common/Nav.js
+++ src/common/Nav.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Nav() {
     return (
         <nav className='navbar w-full'>
             <div>
                 <h1 className='text-2xl'>
+                    <Link to={"/"}>
                     edzest <span className='font-light'>exams</span>
+                    </Link>
                     <div className="tooltip tooltip-right" data-tip="this application is under active development">
                         <div className="badge badge-secondary badge-outline badge-xs align-super m-2">alpha</div>
                     </div>
