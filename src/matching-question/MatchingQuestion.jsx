6fdc0616cdd3e835d7a61a2b929fd41c4a3549b5
Piyush Gupta <piyushpappugupta@gmail.com>
2024-08-03 20:52:24 +0530

    initial integration with matchingQuestion

---
+++ src/matching-question/MatchingQuestion.jsx
@@ -0,0 +1,109 @@
+import React, {useState} from 'react';
+import { motion } from 'framer-motion';
+
+const MatchingQuestion = ({question, updateAnswer}) =>{
+  const [left, setLeft] = useState(question.leftOptions);
+  const [rightOption] = useState(question.rightOptions); // just for showing the option on the right side in background, is never modified by user
+  const [right, setRight] = useState(Array(left.size).fill(null)); // actually stores the data on the right side and keeps modifying
+  const [currentActive, setCurrentActive] = useState(-1); // keeps track on which right droppable is the option dragged over currently
+
+  const handleDragStart = (e, option) => {
+    e.dataTransfer.setData("optionDetail", JSON.stringify(option));
+  };
+
+  const handleDragOver = (e, index) => {
+    e.preventDefault();
+    if (right[index]) return;
+    setCurrentActive(index);
+  };
+
+  const handleDragLeave = (e) => {
+    setCurrentActive(-1);
+  }
+
+  const handleDragEnd = (e, index) => {
+    if (right[index]) return;
+    const optionDetail = JSON.parse(e.dataTransfer.getData("optionDetail"));
+    if (index === rightOption.length) {
+      // dragging to left
+      setLeft((pv) => [...pv, optionDetail]);
+      const rightIndex = right.findIndex((c) => c?.id === optionDetail.id);
+      const newRight = [...right];
+      newRight[rightIndex] = null;
+      setRight(newRight);
+    } else {
+      // dragging to right
+      const newRight = [...right];
+      newRight[index] = optionDetail;
+      setRight(newRight);
+      setLeft((pv) => pv.filter((c) => c.id !== optionDetail.id));
+    }
+    setCurrentActive(-1);
+    updateNewAnswer();
+  }
+
+  const updateNewAnswer = () => {
+    const answers = [];
+    right.forEach((option, index) => {
+      if (option === null) return;
+      answers.add({
+        leftId: rightOption[index].id,
+        rightId: option.id
+      });
+    });
+    updateAnswer(answers);
+  }
+  
+
+  return (
+    <div className="flex">
+      <div className="w-80 shrink-0">
+        <div className={`transition-colors w-full items-center justify-between h-full ${currentActive === rightOption.length ? "border-orange-500 bg-yellow-300 text-orange-500" : "" }`}
+          onDragOver={(e) => handleDragOver(e, rightOption.length)}
+          onDragLeave={handleDragLeave}
+          onDrop={(e) => handleDragEnd(e, rightOption.length)}
+        >
+         {
+            left.map((leftOption) => {
+              return <DraggableOption key={leftOption.id} {...leftOption} handleDragStart={handleDragStart}/>
+            })
+          }
+        </div>   
+      </div>
+      <div className="ml-16 w-80 shrink-0 bg-neutral-300/50">
+        {
+          rightOption.map((rightOption, index) => {
+            return <div key={rightOption.id} className={`relative rounded w-full h-16  flex items-center justify-content border mb-2 p-3 ${currentActive === index ? "border-orange-500 bg-yellow-300 text-orange-500" : "bg-gray-200 border-gray-900"}`}
+              onDragOver={(e) => handleDragOver(e, index)}
+              onDragLeave={handleDragLeave}
+              onDrop={(e) => handleDragEnd(e, index)}
+              >
+                {
+                  right[index] ? (
+                    <DraggableOption key={right[index]?.id} id={right[index]?.id} text={right[index]?.text} handleDragStart={handleDragStart}/>
+                  ) : (
+                    <div className="absolute insert-0 flex items-center justify-center text-gray-900 font-bold opacity-20">{rightOption.text}</div>
+                  )
+                }
+              </div>
+          })
+        }
+      </div>
+    </div>
+  );
+}
+
+const DraggableOption = ({id, text, handleDragStart}) => {
+  return (
+    <motion.div draggable="true"
+      layout
+      layoutId={id}
+      onDragStart={(e) => handleDragStart(e, {id, text})}
+      className="cursor-grab rounded border border-neutral-700 p-3 active:cursor-grabbing mb-2 w-full">
+      <p className="">{text}</p>
+    </motion.div>
+  );
+};
+
+
+export default MatchingQuestion;
--- src/questionnaire/questionnaire.js
+++ src/questionnaire/questionnaire.js
@@ -1,9 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 import { TEST, ASNWER_SHEET } from '../constants/constants';
+import { FiX } from "react-icons/fi";
+import MatchingQuestion from '../matching-question/MatchingQuestion';
 
 function Questionnaire() {
-    const [questions, setQuestions] = useState([]);
+    const [questions, setQuestions] = useState(new Map());
+    const [matchingQuestions, setMatchingQuestions] = useState(new Map());
     const [currentIndex, setCurrentIndex] = useState(0);
     const [answers, setAnswers] = useState({});
     const navigate = useNavigate();
@@ -38,19 +41,32 @@ function Questionnaire() {
         new Promise((resolve, reject) => {
             resolve(TEST[0]);
         })
-            .then(data => setQuestions(data['questions']))
+            .then(data => {
+                const questionsMap = data['questions'].reduce((map, question) => {
+                    map.set(parseInt(question.id), question);
+                    return map;
+                }, new Map());
+                
+                const matchingQuestionsMap = data['matchingQuestions'].reduce((map, matchingQuestion) => {
+                    map.set(parseInt(matchingQuestion.id), matchingQuestion);
+                    return map;
+                }, new Map());
+
+                setQuestions(questionsMap);
+                setMatchingQuestions(matchingQuestionsMap);
+            })
             .then(resetAnswers())
             .catch(error => console.error(error));
     }, [id]);
 
     const handleNext = () => {
         console.log(answers)
-        setCurrentIndex((currentIndex + 1) % questions.length);
+        setCurrentIndex((currentIndex + 1) % questions.size);
     };
 
     const handlePrevious = () => {
         console.log(answers)
-        setCurrentIndex((currentIndex - 1 + questions.length) % questions.length);
+        setCurrentIndex((currentIndex - 1 + questions.size) % questions.size);
     };
 
     const handleClear = () => {
@@ -85,35 +101,55 @@ function Questionnaire() {
             .catch(error => console.error(error));
     };
 
-    if (!questions.length) return <div>Loading...</div>;
+	const handleMatchingQuestionChange = (matchingQuestionAnswer) => {
+        setAnswers({
+            ...answers,
+            [currentIndex + 1]: matchingQuestionAnswer
+        });
+    }
+
+    if (!questions.size) return <div>Loading...</div>;
 
     return (
         <div className='container px-4 mx-auto prose sm:block'>
             <div className='text-right'>
                 <button className='btn btn-ghost btn-circle text-3xl font-light m-4'
-                    onClick={() => document.getElementById('my_modal_1').showModal()}>X</button>
+                    onClick={() => document.getElementById('my_modal_1').showModal()}><FiX /></button>
             </div>
             <hr className='mt-0' />
-            <h4>{`Q ${currentIndex + 1}: ${questions[currentIndex].text}`}</h4>
-            <div className='my-6'>
-                {questions[currentIndex].options.map((option, index) => (
-                    <div key={index}>
-                        <label className='flex my-3'>
-                            <input type="radio"
-                                className='radio radio-primary mr-3'
-                                value={option.id}
-                                checked={answers[currentIndex + 1] === option.id}
-                                onChange={handleOptionChange} />
-                            {option.text}
-                        </label>
-                    </div>
-                ))}
-            </div>
+            {
+                questions.has(currentIndex + 1) ? (
+                <>
+                <h4>{`Q ${currentIndex + 1}: ${questions.get(currentIndex + 1).text}`}</h4>
+                <div className='my-6'>
+                    {questions.get(currentIndex + 1).options.map((option, index) => (
+                        <div key={index}>
+                            <label className='flex my-3'>
+                                <input type="radio"
+                                    className='radio radio-primary mr-3'
+                                    value={option.id}
+                                    checked={answers[currentIndex + 1] === option.id}
+                                    onChange={handleOptionChange} />
+                                {option.text}
+                            </label>
+                        </div>
+                    ))}
+                </div>
+                </>
+                )
+                : (
+                    <>
+                    <h4>{`Q ${currentIndex + 1}: ${matchingQuestions.get(currentIndex + 1).text}`}</h4>
+                    <MatchingQuestion question={matchingQuestions.get(currentIndex + 1)} updateAnswer={handleMatchingQuestionChange} />
+                    </>
+                )
+            }
+            
             <div className='grid-cols-3 grid gap-4 mb-10 md:flex md:justify-between'>
                 <button onClick={handlePrevious} disabled={currentIndex === 0} className='btn md:flex-inital'>Previous</button>
                 <button onClick={handleClear} className='btn btn-ghost'>Clear</button>
-                {currentIndex < questions.length - 1 && <button onClick={handleNext} disabled={currentIndex === questions.length - 1} className='btn btn-primary md:flex-initial'>Next</button>}
-                {currentIndex === questions.length - 1 && <button onClick={handleSubmit} className='btn btn-primary md:flex-initial'>Submit</button>}
+                {currentIndex < questions.size - 1 && <button onClick={handleNext} disabled={currentIndex === questions.size - 1} className='btn btn-primary md:flex-initial'>Next</button>}
+                {currentIndex === questions.size - 1 && <button onClick={handleSubmit} className='btn btn-primary md:flex-initial'>Submit</button>}
             </div>
 
             {/* exit modal */}
